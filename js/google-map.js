807c4e54f3d8ae17f16a0c15d68ba157cf9bda3e
Logan <logandickey1@gmail.com>
2020-08-13 20:57:04 -0700

    Added contact and google maps api

--- js/google-map.js
+++ js/google-map.js
@@ -5,13 +5,12 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
+    var myLatlng = new google.maps.LatLng(37.690910, -121.897656);
+    // 37.690910, -121.897656
+
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 13,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -34,29 +33,21 @@ function init() {
         ]
     };
 
-    
 
-    // Get the HTML DOM element that will contain your map 
+
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
+    var latlng = new google.maps.LatLng(37.690910, -121.897656);
+    new google.maps.Marker({
+        position: latlng,
+        map: map,
+    });
 
-        });
-    }
-    
+
 }
 google.maps.event.addDomListener(window, 'load', init);
---
+++ scripts/contact.php
@@ -0,0 +1,19 @@
+<php
+if(isset($_POST['submit'])) {
+    $to_address="nizesystems@gmail.com";
+    $subject="Nize.education Contact Us";
+    $name=$_POST['name'];
+    $email="nizesystems@gmail.com";
+    $content=$_POST['content'];
+    $message="Name: " .$firstname." ".$lastname."\n";
+    $message .="Email: " .$_POST['email']."\n";
+    $message .="Subject: " .$_POST['subject']."\n";
+    $message .="Content: " .$content."\n";
+
+    $headers=" From : ". $email ."\r\n";
+    $headers.= 'MIME-Version: 1.0' . "\r\n";
+    $headers.= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    mail($to_address, $subject, $message, $headers);
+}
+?>
